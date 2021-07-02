9a9b9c44142798befbadf2903209fe1bb303e726
Stefano Di Cecco <s.dicecco@reply.it>
2021-07-02 17:58:31 +0200

    updated footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,13 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
 
+export interface Tile {
+  id: number;
+  cols: number;
+  rows: number;
+}
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  tiles: Tile[] = [
+    {id: 1, cols: 3, rows: 1},
+    {id: 2, cols: 1, rows: 2},
+    {id: 3, cols: 1, rows: 1},
+    {id: 4, cols: 2, rows: 1},
+  ];
+
   isMobile: boolean = false;
+  loading: boolean = true;
   
   constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
     this.matIconRegistry.addSvgIcon(
@@ -45,6 +59,9 @@ export class FooterComponent implements OnInit {
       // false for not mobile device
       this.isMobile = false;
     }
+    setTimeout(() => {
+      this.loading = false;
+    }, 2500);
   }
 
 }
