d5c6a02a4f2add2a5ecd3a6fddedb446aab5b46d
Stefano Di Cecco <s.dicecco@reply.it>
2021-07-09 11:40:43 +0200

    added method to expert service

--- src/app/shared/services/expert.service.ts
+++ src/app/shared/services/expert.service.ts
@@ -16,10 +16,44 @@ export class ExpertService {
   async getExperts() {
     try {
       const experts = await this.apiService.get(`experts`);
-      console.log(experts)
+      console.log(experts);
       this.experts$.next(experts);
     } catch (err) {
       console.log('Could not load experts.', err);
     }
+  }
+
+  async getExpert(expertId: number) {
+    return await this.apiService.get(`/experts/${expertId}`);
+  }
+
+  async selectExpert(expertId: number) {
+    if (this.experts$.value && this.experts$.value.length > 0) {
+      const expert = this.experts$.value.find((expert) => {
+        return expert.id === expertId;
+      });
+      this.selectedExpert$.next(expert);
+    }
+  }
+
+  async refetchExperts() {
+    if (this.selectedExpert$.value) {
+      await this.getExpert(this.selectedExpert$.value.id);
+    }
+  }
+
+  async createExpert(expert: Expert) {
+    try {
+      const createdExpert = await this.apiService.post('experts', expert);
+      return createdExpert;
+    } catch (err) {
+      console.log(`Could not Create Expert`, err);
+      throw err;
+    }
+  }
+
+  async deleteExpert(expertId: number) {
+    const deletedExpert = await this.apiService.delete(`experts/${expertId}`);
+    return deletedExpert;
   }
 }
