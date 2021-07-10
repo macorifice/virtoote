fee4c1e556d353751e0deba614097829c279fefc
Stefano Di Cecco <s.dicecco@reply.it>
2021-07-10 16:08:57 +0200

    updated expert desktop view

--- src/app/components/expert/expert.component.ts
+++ src/app/components/expert/expert.component.ts
@@ -4,18 +4,34 @@ import { Subscription } from 'rxjs';
 import { Expert } from 'src/app/shared/model/expert.model';
 import { ExpertService } from 'src/app/shared/services/expert.service';
 
+export interface Tile {
+  id: number;
+  color: string;
+  cols: number;
+  rows: number;
+}
+
 @Component({
   selector: 'app-expert',
   templateUrl: './expert.component.html',
   styleUrls: ['./expert.component.scss'],
 })
 export class ExpertComponent implements OnInit {
+  tiles: Tile[] = [
+    {id: 1, cols: 3, rows: 1, color: 'transparent'},
+    {id: 2, cols: 1, rows: 3, color: 'transparent'},
+    {id: 3, cols: 1, rows: 3, color: 'transparent'},
+    {id: 4, cols: 2, rows: 3, color: 'transparent'},
+  ];
+
   expertIdFromRoute: string;
   expert: any;
   experts: Expert[] = [];
 
   loading: boolean = true;
   subscriptions: Subscription[] = [];
+
+  isMobile: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -31,5 +47,17 @@ export class ExpertComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      // true for mobile device
+      this.isMobile = true;
+    } else {
+      // false for not mobile device
+      this.isMobile = false;
+    }
+  }
 }
